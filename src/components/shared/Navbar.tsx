72d81c6c773fc529d88f8e8cd550818a42bed4e7
Hasnaat Iftikhar <hasnaatfreelancing@gmail.com>
2023-09-26 17:19:59 +0500

    FEAT: Worked on nav styes

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -5,7 +5,7 @@ import Container from "./Container";
 
 const Navbar = async () => {
   return (
-    <header className="py-3 border-b border-[#f3f3f3]">
+    <header className="py-5 border-b border-[#f3f3f3]">
       <Container>
         <div className="flex justify-between items-center">
           <h1 className="text-black bg-primary text-base font-medium py-2 px-4 rounded-lg cursor-default">
