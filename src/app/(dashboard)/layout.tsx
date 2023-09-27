d3ed73b0cac173303831871718afb5d7be495775
Hasnaat Iftikhar <hasnaatfreelancing@gmail.com>
2023-09-28 03:40:35 +0500

    BUG: Bug fixing

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,4 @@
 import { FC, ReactNode } from "react";
-import { Inter } from "next/font/google";
 
 // Components
 import Sidebar from "@/components/shared/Sidebar";
@@ -7,26 +6,20 @@ import Container from "@/components/shared/Container";
 import Navbar from "@/components/shared/Navbar";
 import CommunitySidebar from "@/components/shared/CommunitySidebar";
 
-const inter = Inter({ subsets: ["latin"] });
-
 interface PropTypes {
   children: ReactNode;
 }
 
 const Layout: FC<PropTypes> = async ({ children }) => {
   return (
-    <html>
-      <body className={inter.className}>
-        <main className="h-screen flex flex-col">
-          <Navbar />
-          <Container className="flex-1 flex flex-row">
-            <Sidebar className="pt-[20px] w-[250px] pr-[20px] border-r border-[rgba(0,0,0, 0.1)]" />
-            <div className="p-[20px] flex-1 h-full">{children}</div>
-            <CommunitySidebar className="pt-[20px] w-[300px] pl-[20px] border-l border-[rgba(0,0,0, 0.1)]" />
-          </Container>
-        </main>
-      </body>
-    </html>
+    <main className="h-screen flex flex-col">
+      <Navbar />
+      <Container className="flex-1 flex flex-row">
+        <Sidebar className="pt-[20px] w-[250px] pr-[20px] border-r border-[rgba(0,0,0, 0.1)]" />
+        <div className="p-[20px] flex-1 h-full">{children}</div>
+        <CommunitySidebar className="pt-[20px] w-[300px] pl-[20px] border-l border-[rgba(0,0,0, 0.1)]" />
+      </Container>
+    </main>
   );
 };
 
