1b9770896bb837e8178e8770723c9eb1bacf9c83
Hasnaat Iftikhar <hasnaatfreelancing@gmail.com>
2023-09-28 02:16:12 +0500

    BUG: Worked on bug fixing

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -23,32 +23,28 @@ interface PropTypes {
 const Layout: FC<PropTypes> = ({ children }) => {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>
-          <main>
-            <header className="fixed top-[20px] w-full">
-              <Container>
-                <BackButton />
-              </Container>
-            </header>
-            <section className="h-screen flex flex-row justify-between items-center">
-              <div className="max-w-[400px] mx-auto flex flex-col justify-center items-start gap-[14px]">
-                <h2 className="text-[24px] font-medium">
-                  Connect, Share, and Explore!
-                </h2>
-                <p className="text-[16px] leading-[20px]">
-                  Welcome to our social media platform where you can connect
-                  with friends, share moments, and explore the world. Join the
-                  community today!
-                </p>
-              </div>
-              <div className="w-[600px] h-full bg-primary flex justify-center items-center">
-                {children}
-              </div>
-            </section>
-          </main>
-        </body>
-      </html>
+      <main className={inter.className}>
+        <header className="fixed top-[20px] w-full">
+          <Container>
+            <BackButton />
+          </Container>
+        </header>
+        <section className="h-screen flex flex-row justify-between items-center">
+          <div className="max-w-[400px] mx-auto flex flex-col justify-center items-start gap-[14px]">
+            <h2 className="text-[24px] font-medium">
+              Connect, Share, and Explore!
+            </h2>
+            <p className="text-[16px] leading-[20px]">
+              Welcome to our social media platform where you can connect with
+              friends, share moments, and explore the world. Join the community
+              today!
+            </p>
+          </div>
+          <div className="w-[600px] h-full bg-primary flex justify-center items-center">
+            {children}
+          </div>
+        </section>
+      </main>
     </ClerkProvider>
   );
 };
